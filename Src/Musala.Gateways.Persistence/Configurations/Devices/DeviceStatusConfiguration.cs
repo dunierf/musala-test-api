82daadad5744a88faac501529c781803df496cdb
Duniesqui Rodriguez <dunierf@gmail.com>
2023-05-12 00:44:05 -0400

    Fluent Api configurations

---
+++ Src/Musala.Gateways.Persistence/Configurations/Devices/DeviceStatusConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Musala.Gateways.Domain.Entities.Devices;
+
+namespace Musala.Gateways.Persistence.Configurations.Devices
+{
+    public class DeviceStatusConfiguration : IEntityTypeConfiguration<DeviceStatus>
+    {
+        public void Configure(EntityTypeBuilder<DeviceStatus> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .HasMaxLength(50);
+
+            builder.Property(c => c.Id)
+                .UseIdentityColumn();
+
+            builder.Property(c => c.CreatedDate)
+                .IsRequired(true)
+                .HasDefaultValueSql("GETDATE()");
+
+            builder.Property(c => c.ModifiedDate)
+                .IsRequired(true)
+                .HasDefaultValueSql("GETDATE()");
+        }
+    }
+}
