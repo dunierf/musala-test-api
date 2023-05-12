ece50db79de5fdc632216f1f1ffcb5a2e9b0f79d
Duniesqui Rodriguez <dunierf@gmail.com>
2023-05-12 00:53:48 -0400

    Seeding device status

--- Src/Musala.Gateways.Persistence/Seeders/AppDbContextSeeder.cs
+++ Src/Musala.Gateways.Persistence/Seeders/AppDbContextSeeder.cs
@@ -1,4 +1,6 @@
-﻿using Musala.Gateways.Persistence.Contexts;
+﻿using Musala.Gateways.Domain.Entities.Devices;
+using Musala.Gateways.Persistence.Contexts;
+using System.Linq;
 
 namespace Musala.Gateways.Persistence.Seeders
 {
@@ -7,10 +9,20 @@ namespace Musala.Gateways.Persistence.Seeders
 
         public void SeedEverything(AppDbContext db)
         { 
-            db.Database.EnsureCreated();    
+            db.Database.EnsureCreated();
+            SeedDeviceStatus(db);
         }
 
-        // Add your own seed methods 
+        private void SeedDeviceStatus(AppDbContext db)
+        {
+            if (!db.DeviceStatus.Any())
+            {
+                db.DeviceStatus.AddRange(
+                    new DeviceStatus { Id = 1, Name = "Online" },
+                    new DeviceStatus { Id = 2, Name = "Offline" }
+                );
+            }
+        }
 
     }
 }
