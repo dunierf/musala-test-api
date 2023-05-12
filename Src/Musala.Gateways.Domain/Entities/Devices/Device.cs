170fc3375048540374c8dd9ac84bf3b2b557a57d
Duniesqui Rodriguez <dunierf@gmail.com>
2023-05-12 00:19:27 -0400

    Entities

---
+++ Src/Musala.Gateways.Domain/Entities/Devices/Device.cs
@@ -0,0 +1,21 @@
+﻿using Musala.Gateways.Domain.Common;
+using Musala.Gateways.Domain.Entities.Gateways;
+
+namespace Musala.Gateways.Domain.Entities.Devices
+{
+    public class Device : BaseEntity
+    {
+        public int UId { get; set; }
+
+        public string Vendor { get; set; }
+
+        public int StatusId { get; set; }
+
+        public DeviceStatus Status { get; set; }
+
+        public int GatewayId { get; set; }
+
+        public Gateway Gateway { get; set; }
+
+    }
+}
