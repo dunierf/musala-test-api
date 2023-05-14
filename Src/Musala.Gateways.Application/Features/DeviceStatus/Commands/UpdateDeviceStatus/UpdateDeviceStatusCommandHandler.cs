30bb138b4ef247b253fc6ee406ccc6ec82a20996
Duniesqui Rodriguez <dunierf@gmail.com>
2023-05-14 10:57:42 -0400

    Update DeviceStatus command

---
+++ Src/Musala.Gateways.Application/Features/DeviceStatus/Commands/UpdateDeviceStatus/UpdateDeviceStatusCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Musala.Gateways.Application.Common.Exceptions;
+using Musala.Gateways.Application.Features.DeviceStatus.Queries.GetAllDeviceStatus;
+using Musala.Gateways.Persistence.Contexts;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Musala.Gateways.Application.Features.DeviceStatus.Commands.UpdateDeviceStatus
+{
+    public class UpdateDeviceStatusCommandHandler : IRequestHandler<UpdateDeviceStatusCommand, DeviceStatusDto>
+    {
+        private ILogger logger;
+        private AppDbContext db;
+        private IMapper mapper;
+
+        public UpdateDeviceStatusCommandHandler(ILogger<UpdateDeviceStatusCommandHandler> logger, AppDbContext db, IMapper mapper)
+        {
+            this.logger = logger;
+            this.db = db;
+            this.mapper = mapper;
+        }
+
+        public async Task<DeviceStatusDto> Handle(UpdateDeviceStatusCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await db.DeviceStatus.FindAsync(request.Id);
+
+            if (entity == null)
+                throw new NotFoundException(nameof(DeviceStatus), request.Id);
+
+            entity.Name = request.Name;
+            entity.ModifiedDate = DateTime.UtcNow;
+            entity.LastModifiedBy = "system";
+            db.DeviceStatus.Update(entity);
+
+            await db.SaveChangesAsync(cancellationToken);
+            return mapper.Map<DeviceStatusDto>(entity);
+        }
+    }
+}
---
+++ Src/Musala.Gateways.Application/Features/DeviceStatus/Commands/UpdateDeviceStatus/UpdateDeviceStatusCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Musala.Gateways.Application.Features.DeviceStatus.Commands.UpdateDeviceStatus
+{
+    public class UpdateDeviceStatusCommandValidator : AbstractValidator<UpdateDeviceStatusCommand>
+    {
+        public UpdateDeviceStatusCommandValidator()
+        {
+            RuleFor(c => c.Id)
+                .NotEmpty()
+                .NotNull()
+                .NotEqual(0)
+                .WithMessage("Id is required");
+
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .NotNull()
+                .NotEqual("")
+                .WithMessage("Name is required");
+        }
+    }
+}
