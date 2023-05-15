d4b8368096eebe22ae723681f7233c152ba1daa6
Duniesqui Rodriguez <dunierf@gmail.com>
2023-05-14 23:37:50 -0400

    Update Device command

---
+++ Src/Musala.Gateways.Application/Features/Devices/Commands/UpdateDevice/UpdateDeviceCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Musala.Gateways.Application.Common.Exceptions;
+using Musala.Gateways.Application.Features.Devices.Queries.GetAllDevices;
+using Musala.Gateways.Domain.Entities.Gateways;
+using Musala.Gateways.Domain.Enums;
+using Musala.Gateways.Persistence.Contexts;
+using System.Numerics;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Musala.Gateways.Application.Features.TodoItems.Queries.GetAllTodoItems;
+
+namespace Musala.Gateways.Application.Features.Devices.Commands.UpdateDevice
+{
+    public class UpdateDeviceCommandHandler : IRequestHandler<UpdateDeviceCommand, DeviceDto>
+    {
+        private ILogger logger;
+        private AppDbContext db;
+        private IMapper mapper;
+
+        public UpdateDeviceCommandHandler(ILogger<UpdateDeviceCommandHandler> logger, AppDbContext db, IMapper mapper)
+        {
+            this.logger = logger;
+            this.db = db;
+            this.mapper = mapper;
+        }
+
+        public async Task<DeviceDto> Handle(UpdateDeviceCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await db.Devices.FindAsync(request.Id);
+
+            if (entity == null)
+                throw new NotFoundException(nameof(Devices), request.Id);
+
+            entity.UId = request.UId;
+            entity.Vendor = request.Vendor;
+            entity.StatusId = request.StatusId;
+            entity.GatewayId = request.GatewayId;
+            entity.LastModifiedBy = "system";
+            entity.ModifiedDate = DateTime.UtcNow;
+
+            db.Devices.Update(entity);
+            await db.SaveChangesAsync(cancellationToken);
+
+            return mapper.Map<DeviceDto>(entity);
+        }
+    }
+}
---
+++ Src/Musala.Gateways.Application/Features/Devices/Commands/UpdateDevice/UpdateDeviceCommandValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+
+namespace Musala.Gateways.Application.Features.Devices.Commands.UpdateDevice
+{
+    public class UpdateDeviceCommandValidator : AbstractValidator<UpdateDeviceCommand>
+    {
+        public UpdateDeviceCommandValidator()
+        {
+            RuleFor(c => c.UId)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("UId is required");
+
+            RuleFor(c => c.Vendor)
+                .NotEmpty()
+                .NotNull()
+                .NotEqual("")
+                .WithMessage("Name is required");
+
+            RuleFor(c => c.StatusId)
+                .NotEmpty()
+                .NotNull()
+                .NotEqual(0)
+                .WithMessage("SatusId is required");
+
+            RuleFor(c => c.GatewayId)
+                .NotEmpty()
+                .NotNull()
+                .NotEqual(0)
+                .WithMessage("GatewayId is required");
+        }
+    }
+}
