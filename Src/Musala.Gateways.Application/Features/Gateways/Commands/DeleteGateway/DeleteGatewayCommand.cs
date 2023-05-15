268ca901e25fb3eb4f19737e65c23c977321dc82
Duniesqui Rodriguez <dunierf@gmail.com>
2023-05-14 21:40:01 -0400

    Delete Gateway command

---
+++ Src/Musala.Gateways.Application/Features/Gateways/Commands/DeleteGateway/DeleteGatewayCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Musala.Gateways.Application.Features.Gateways.Commands.DeleteGateway
+{
+    public class DeleteGatewayCommand : IRequest
+    {
+        public int Id { set; get; }
+    }
+}
---
+++ Src/Musala.Gateways.Application/Features/Gateways/Commands/DeleteGateway/DeleteGatewayCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Musala.Gateways.Application.Features.Gateways.Commands.DeleteGateway
+{
+    public class DeleteGatewayCommandValidator : AbstractValidator<DeleteGatewayCommand>
+    {
+        public DeleteGatewayCommandValidator()
+        {
+            RuleFor(c => c.Id)
+                .NotEmpty()
+                .NotNull()
+                .NotEqual(0)
+                .WithMessage("Id is required");
+        }
+    }
+}
