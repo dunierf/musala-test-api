5dd9ac31dae73144c418066e461fbcfa5978eb66
Duniesqui Rodriguez <dunierf@gmail.com>
2023-05-14 23:22:28 -0400

    Delete Device command

---
+++ Src/Musala.Gateways.Application/Features/Devices/Commands/DeleteDevice/DeleteDeviceCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Musala.Gateways.Application.Features.Devices.Commands.DeleteDevice
+{
+    public class DeleteDeviceCommandValidator : AbstractValidator<DeleteDeviceCommand>
+    {
+        public DeleteDeviceCommandValidator()
+        {
+            RuleFor(c => c.Id)
+                .NotEmpty()
+                .NotNull()
+                .NotEqual(0)
+                .WithMessage("Id is required");
+        }
+    }
+}
